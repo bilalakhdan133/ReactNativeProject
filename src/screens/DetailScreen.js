e6013bee1b39009e29e2f5414f6e631874dd691f
Muhammad Nur Habibi <awie.mnh@gmail.com>
2024-02-06 10:37:49 +0700

    terbaru

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,13 +1,45 @@
-// components/DetailsScreen/js
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
-const DetailScreen = () => {
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Details Screen</Text>
+    <View style={styles.container}>
+      <Image source={require("../assets/img/bilal.jpg")} style={styles.logo} />
+      <Text style={styles.title}>GANTENG ABANG DEKK</Text>
+      {/* <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Tekan Saya</Text>
+      </TouchableOpacity> */}
     </View>
   );
 };
 
-export default DetailScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  logo: {
+    width: 350,
+    height: 350,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#3498db",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default App;
--- src/screens/homeScreen.js
+++ src/screens/homeScreen.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { View, Text, Button } from "react-native";
 
@@ -10,45 +9,8 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Details"
         onPress={() => navigation.navigate("Details")}
       />
-=======
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import Carousel from 'react-native-snap-carousel';
-
-const HomeScreen = ({ navigation }) => {
-    const carouselData = [
-        { id: 1, source: require('./src/assets/image1.jpg'), text: 'Lorem ipsum 1' },
-        { id: 2, source: require('./src/assetsimage2.jpg'), text: 'Lorem ipsum 2' },
-      ];
-    
-      const renderCarouselItem = ({ item }) => (
-        <Image source={item.source} style={{ width: 300, height: 200, borderRadius: 10 }} />
-      );
-    
-
-    return (
-
-        
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-
-            {/* Image Carousel */}
-        <View style={{ marginVertical: 20 }}>
-        <Carousel
-          data={carouselData}
-          renderItem={renderCarouselItem}
-          sliderWidth={350}
-          itemWidth={300}
-        />
-        </View>
-        <Text>Home Screen</Text>
-        <Button
-            title="Go to Details"
-            onPress={() => navigation.navigate('Details')}
-        />
->>>>>>> 54c98412151c75042876107c51ece738caccf389
     </View>
   );
 };
-
 
 export default HomeScreen;
