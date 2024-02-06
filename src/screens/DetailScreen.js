642ef7194bb8b5b8d06d0d577e4fcad6d402d303
Muhammad Nur Habibi <awie.mnh@gmail.com>
2024-02-06 10:22:07 +0700

    update confflik1

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,60 +1,13 @@
 // components/DetailsScreen/js
-// import React from "react";
-// import { View, Text, Button } from "react-native";
-
-// const DetailScreen = () => {
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>Details Screen</Text>
-//     </View>
-//   );
-// };
-
-// export default DetailScreen;
-
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const App = () => {
+const DetailScreen = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require("../assets/img/bilal.jpg")} style={styles.logo} />
-      <Text style={styles.title}>GANTENG ABANG DEKK</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Details Screen</Text>
     </View>
   );
 };
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f0f0f0",
-  },
-  logo: {
-    width: 300,
-    height: 300,
-    marginBottom: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: "#3498db",
-    padding: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
-
-export default App;
-=======
 export default DetailScreen;
->>>>>>> 82152c35d03a45497d43a0b0fdf0c4776b9dbeae
