f4f2a709f3ea23fb466baef07d914968aa14685b
Arva Firjatullah <arvafg07@gmail.com>
2024-02-06 10:45:40 +0700

    update foto bilal

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,13 +1,34 @@
 // components/DetailsScreen/js
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
 
 const DetailScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Details Screen</Text>
+    <View style={[styles.container]}>
+      <Image source={require("../assets/bilal.jpeg")} style={styles.image} />
+      <Text style={styles.judul}>Details Screen</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 200,
+  },
+
+  judul: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  image: {
+    width: 350,
+    height: 350,
+    marginBottom: 20,
+  },
+});
 
 export default DetailScreen;
